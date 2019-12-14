aa3a8adc0f019d2715e2e7935d8f8743ab7d520d
scottdao <13466484073@139.com>
2019-12-14 18:40:47 +0800

    .

--- egg-servers/app/router.js
+++ egg-servers/app/router.js
@@ -1,14 +1,11 @@
-'use strict';
+"use strict";
 
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-    const {
-        router,
-        controller
-    } = app;
-    //console.log(router);
-    console.log(controller);
-    router.get('/', controller.home.index);
+  const { router, controller } = app;
+  //console.log(router);
+  // console.log(controller);
+  router.get("/", controller.home.index);
 };
